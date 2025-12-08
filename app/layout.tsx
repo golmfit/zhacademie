3389fe57d8bd37a5bc592a04f13bb8be5eaf6688
Yassine Harrati <harrati.yassine2002@gmail.com>
2025-12-08 18:52:01 +0100

    maintenance

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,56 @@
-import type React from "react"
-import "./globals.css"
-import { Inter } from "next/font/google"
-import { AuthProvider } from "@/contexts/auth-context"
-import { ThemeProvider } from "@/components/theme-provider"
-import MarketingNavbar from "@/components/MarketingNavbar";
-import MarketingFooter from "@/components/MarketingFooter";
-const inter = Inter({ subsets: ["latin"] })
+// import type React from "react"
+// import "./globals.css"
+// import { Inter } from "next/font/google"
+// import { AuthProvider } from "@/contexts/auth-context"
+// import { ThemeProvider } from "@/components/theme-provider"
+// import MarketingNavbar from "@/components/MarketingNavbar";
+// import MarketingFooter from "@/components/MarketingFooter";
+// const inter = Inter({ subsets: ["latin"] })
+
+// export const metadata = {
+//   title: "ZHAcademie - Platform",
+//   description: "Streamline your student visa application process with ZHAcademie",
+//     generator: 'v0.dev',
+//     icons: {
+//       icon: "/zhicon.png"
+// }
+
+// }
+
+// export default function RootLayout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <html lang="en">
+//       <head>
+//         <link rel="icon" href="/app/zh.png" />
+        
+//       </head>
+//       <body className={inter.className}>
+//         <MarketingNavbar />
+//         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+//           <AuthProvider>{children}</AuthProvider>
+//         </ThemeProvider>
+//         <MarketingFooter />
+//       </body>
+//     </html>
+//   )
+// }
+
+
+import "./globals.css";
+import Maintenance from "./maintenance";
 
 export const metadata = {
-  title: "ZHAcademie - Platform",
-  description: "Streamline your student visa application process with ZHAcademie",
-    generator: 'v0.dev',
-    icons: {
-      icon: "/zhicon.png"
-}
+  title: "Site Under Construction",
+};
 
-}
+export default function RootLayout({ children }) {
+  const maintenanceMode = true; // switch OFF when ready
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/app/zh.png" />
-        
-      </head>
-      <body className={inter.className}>
-        <MarketingNavbar />
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-          <AuthProvider>{children}</AuthProvider>
-        </ThemeProvider>
-        <MarketingFooter />
+      <body>
+        {maintenanceMode ? <Maintenance /> : children}
       </body>
     </html>
-  )
+  );
 }
