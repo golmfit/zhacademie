2a4a6c0dcf0b03246f642e250ed33afed466a0ff
Yassine Harrati <harrati.yassine2002@gmail.com>
2025-12-19 15:54:28 +0100

    end of the constractions

--- app/maintenance.tsx
+++ app/maintenance.tsx
@@ -1,19 +1,19 @@
-export default function Maintenance() {
-  return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        textAlign: "center",
-        fontFamily: "sans-serif",
-        padding: "20px"
-      }}
-    >
-      <h1>🚧 Website Under Construction</h1>
-      <p>We're currently working on improvements.</p>
-    </div>
-  );
-}
+// export default function Maintenance() {
+//   return (
+//     <div
+//       style={{
+//         height: "100vh",
+//         display: "flex",
+//         justifyContent: "center",
+//         alignItems: "center",
+//         flexDirection: "column",
+//         textAlign: "center",
+//         fontFamily: "sans-serif",
+//         padding: "20px"
+//       }}
+//     >
+//       <h1>🚧 Website Under Construction</h1>
+//       <p>We're currently working on improvements.</p>
+//     </div>
+//   );
+// }
